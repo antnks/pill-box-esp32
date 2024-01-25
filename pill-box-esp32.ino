9df13a31f6d6a21d8a11fd65ad96e10e830a60f9
antnks <antnks@users.noreply.github.com>
2024-01-25 08:31:27 +0200

    Moved boilerplate code to a header file

--- pill-box-esp32.ino
+++ pill-box-esp32.ino
@@ -4,29 +4,11 @@
 #include <HTTPClient.h>
 #include <WiFiClientSecure.h>
 
-#define HOUR 3600000 //60*60*1000
-#define RED  15
-#define HAL  13
-#define WIFISSID "mywifi"
-#define WIFIPSK  "mypass"
-#define CONFURL "https://myserver/config"
-#define LOGURL "https://myserver/script"
-
-unsigned int morning;
-unsigned int evening;
+#include "config.h"
 
 unsigned long lastOpen;
-unsigned long cooldown = 3000;
-unsigned long open_too_long = 10000;
-unsigned long day_pill_interval = (evening - morning) * HOUR;
-unsigned long night_pill_interval = (morning + (24 - evening)) * HOUR;
 int state;
-int CLOSED = 0;
-int OPENED = 1;
-int OVERDUE = 2;
-
 WiFiMulti WiFiMulti;
-const char* rootCACertificate;
 
 void setup()
 {
@@ -85,7 +67,7 @@ void loop()
   // blink if open for too long
   if(state == OPENED && digitalRead(HAL) == OPENED && millis() > lastOpen + open_too_long)
   {
-    blink();
+    blink1();
   }
 
   // blink even more if overdue
@@ -95,166 +77,4 @@ void loop()
   }
 
   delay(500);
-}
-
-unsigned int get_hour()
-{
-  time_t now;
-  struct tm timeinfo;
-
-  time(&now);
-  setenv("TZ", "EET-2EEST,M3.5.0/3,M10.5.0/4", 1);
-  tzset();
-  localtime_r(&now, &timeinfo);
-
-  return timeinfo.tm_hour;
-}
-
-String upload_data(char *URL)
-{
-  WiFiClientSecure *client = new WiFiClientSecure;
-  if(client)
-  {
-    // cloudflare
-    rootCACertificate = \
-    /*
-    "-----BEGIN CERTIFICATE-----\n" \
-    "MIIDzTCCArWgAwIBAgIQCjeHZF5ftIwiTv0b7RQMPDANBgkqhkiG9w0BAQsFADBa\n" \
-    "MQswCQYDVQQGEwJJRTESMBAGA1UEChMJQmFsdGltb3JlMRMwEQYDVQQLEwpDeWJl\n" \
-    "clRydXN0MSIwIAYDVQQDExlCYWx0aW1vcmUgQ3liZXJUcnVzdCBSb290MB4XDTIw\n" \
-    "MDEyNzEyNDgwOFoXDTI0MTIzMTIzNTk1OVowSjELMAkGA1UEBhMCVVMxGTAXBgNV\n" \
-    "BAoTEENsb3VkZmxhcmUsIEluYy4xIDAeBgNVBAMTF0Nsb3VkZmxhcmUgSW5jIEVD\n" \
-    "QyBDQS0zMFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEua1NZpkUC0bsH4HRKlAe\n" \
-    "nQMVLzQSfS2WuIg4m4Vfj7+7Te9hRsTJc9QkT+DuHM5ss1FxL2ruTAUJd9NyYqSb\n" \
-    "16OCAWgwggFkMB0GA1UdDgQWBBSlzjfq67B1DpRniLRF+tkkEIeWHzAfBgNVHSME\n" \
-    "GDAWgBTlnVkwgkdYzKz6CFQ2hns6tQRN8DAOBgNVHQ8BAf8EBAMCAYYwHQYDVR0l\n" \
-    "BBYwFAYIKwYBBQUHAwEGCCsGAQUFBwMCMBIGA1UdEwEB/wQIMAYBAf8CAQAwNAYI\n" \
-    "KwYBBQUHAQEEKDAmMCQGCCsGAQUFBzABhhhodHRwOi8vb2NzcC5kaWdpY2VydC5j\n" \
-    "b20wOgYDVR0fBDMwMTAvoC2gK4YpaHR0cDovL2NybDMuZGlnaWNlcnQuY29tL09t\n" \
-    "bmlyb290MjAyNS5jcmwwbQYDVR0gBGYwZDA3BglghkgBhv1sAQEwKjAoBggrBgEF\n" \
-    "BQcCARYcaHR0cHM6Ly93d3cuZGlnaWNlcnQuY29tL0NQUzALBglghkgBhv1sAQIw\n" \
-    "CAYGZ4EMAQIBMAgGBmeBDAECAjAIBgZngQwBAgMwDQYJKoZIhvcNAQELBQADggEB\n" \
-    "AAUkHd0bsCrrmNaF4zlNXmtXnYJX/OvoMaJXkGUFvhZEOFp3ArnPEELG4ZKk40Un\n" \
-    "+ABHLGioVplTVI+tnkDB0A+21w0LOEhsUCxJkAZbZB2LzEgwLt4I4ptJIsCSDBFe\n" \
-    "lpKU1fwg3FZs5ZKTv3ocwDfjhUkV+ivhdDkYD7fa86JXWGBPzI6UAPxGezQxPk1H\n" \
-    "goE6y/SJXQ7vTQ1unBuCJN0yJV0ReFEQPaA1IwQvZW+cwdFD19Ae8zFnWSfda9J1\n" \
-    "CZMRJCQUzym+5iPDuI9yP+kHyCREU3qzuWFloUwOxkgAyXVjBYdwRVKD05WdRerw\n" \
-    "6DEdfgkfCv4+3ao8XnTSrLE=\n" \
-    "-----END CERTIFICATE-----\n";
-    */
-    // lets encrypt
-      "-----BEGIN CERTIFICATE-----\n" \
-      "MIIFFjCCAv6gAwIBAgIRAJErCErPDBinU/bWLiWnX1owDQYJKoZIhvcNAQELBQAw\n" \
-      "TzELMAkGA1UEBhMCVVMxKTAnBgNVBAoTIEludGVybmV0IFNlY3VyaXR5IFJlc2Vh\n" \
-      "cmNoIEdyb3VwMRUwEwYDVQQDEwxJU1JHIFJvb3QgWDEwHhcNMjAwOTA0MDAwMDAw\n" \
-      "WhcNMjUwOTE1MTYwMDAwWjAyMQswCQYDVQQGEwJVUzEWMBQGA1UEChMNTGV0J3Mg\n" \
-      "RW5jcnlwdDELMAkGA1UEAxMCUjMwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEK\n" \
-      "AoIBAQC7AhUozPaglNMPEuyNVZLD+ILxmaZ6QoinXSaqtSu5xUyxr45r+XXIo9cP\n" \
-      "R5QUVTVXjJ6oojkZ9YI8QqlObvU7wy7bjcCwXPNZOOftz2nwWgsbvsCUJCWH+jdx\n" \
-      "sxPnHKzhm+/b5DtFUkWWqcFTzjTIUu61ru2P3mBw4qVUq7ZtDpelQDRrK9O8Zutm\n" \
-      "NHz6a4uPVymZ+DAXXbpyb/uBxa3Shlg9F8fnCbvxK/eG3MHacV3URuPMrSXBiLxg\n" \
-      "Z3Vms/EY96Jc5lP/Ooi2R6X/ExjqmAl3P51T+c8B5fWmcBcUr2Ok/5mzk53cU6cG\n" \
-      "/kiFHaFpriV1uxPMUgP17VGhi9sVAgMBAAGjggEIMIIBBDAOBgNVHQ8BAf8EBAMC\n" \
-      "AYYwHQYDVR0lBBYwFAYIKwYBBQUHAwIGCCsGAQUFBwMBMBIGA1UdEwEB/wQIMAYB\n" \
-      "Af8CAQAwHQYDVR0OBBYEFBQusxe3WFbLrlAJQOYfr52LFMLGMB8GA1UdIwQYMBaA\n" \
-      "FHm0WeZ7tuXkAXOACIjIGlj26ZtuMDIGCCsGAQUFBwEBBCYwJDAiBggrBgEFBQcw\n" \
-      "AoYWaHR0cDovL3gxLmkubGVuY3Iub3JnLzAnBgNVHR8EIDAeMBygGqAYhhZodHRw\n" \
-      "Oi8veDEuYy5sZW5jci5vcmcvMCIGA1UdIAQbMBkwCAYGZ4EMAQIBMA0GCysGAQQB\n" \
-      "gt8TAQEBMA0GCSqGSIb3DQEBCwUAA4ICAQCFyk5HPqP3hUSFvNVneLKYY611TR6W\n" \
-      "PTNlclQtgaDqw+34IL9fzLdwALduO/ZelN7kIJ+m74uyA+eitRY8kc607TkC53wl\n" \
-      "ikfmZW4/RvTZ8M6UK+5UzhK8jCdLuMGYL6KvzXGRSgi3yLgjewQtCPkIVz6D2QQz\n" \
-      "CkcheAmCJ8MqyJu5zlzyZMjAvnnAT45tRAxekrsu94sQ4egdRCnbWSDtY7kh+BIm\n" \
-      "lJNXoB1lBMEKIq4QDUOXoRgffuDghje1WrG9ML+Hbisq/yFOGwXD9RiX8F6sw6W4\n" \
-      "avAuvDszue5L3sz85K+EC4Y/wFVDNvZo4TYXao6Z0f+lQKc0t8DQYzk1OXVu8rp2\n" \
-      "yJMC6alLbBfODALZvYH7n7do1AZls4I9d1P4jnkDrQoxB3UqQ9hVl3LEKQ73xF1O\n" \
-      "yK5GhDDX8oVfGKF5u+decIsH4YaTw7mP3GFxJSqv3+0lUFJoi5Lc5da149p90Ids\n" \
-      "hCExroL1+7mryIkXPeFM5TgO9r0rvZaBFOvV2z0gp35Z0+L4WPlbuEjN/lxPFin+\n" \
-      "HlUjr8gRsI3qfJOQFy/9rKIJR0Y/8Omwt/8oTWgy1mdeHmmjk7j1nYsvC9JSQ6Zv\n" \
-      "MldlTTKB3zhThV1+XWYp6rjd5JW1zbVWEkLNxE7GJThEUG3szgBVGP7pSWTUTsqX\n" \
-      "nLRbwHOoq7hHwg==\n" \
-      "-----END CERTIFICATE-----\n";
-
-    client->setCACert(rootCACertificate);
-
-    {
-      HTTPClient https;
-  
-      //Serial.print("https begin\n");
-      if (https.begin(*client, URL))
-      {
-        //Serial.print("GET\n");
-        int httpCode = https.GET();
-  
-        if (httpCode > 0)
-        {
-          //Serial.printf("Code: %d\n", httpCode);
-  
-          if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY)
-          {
-            String payload = https.getString();
-            return payload;
-            //Serial.println(payload);
-          }
-        }
-        else
-        {
-          Serial.printf("error: %s\n", https.errorToString(httpCode).c_str());
-        }
-  
-        https.end();
-      }
-      else
-      {
-        Serial.printf("Unable to connect\n");
-      }
-    }
-  
-    delete client;
-  }
-  else
-  {
-    Serial.println("Unable to create client");
-  }
-  return "";
-}
-
-void setNtp()
-{
-  configTime(0, 0, "pool.ntp.org");
-
-  Serial.print("Waiting for NTP time sync: ");
-  time_t nowSecs = 0;
-  while (nowSecs < 8 * 3600 * 2)
-  {
-    delay(500);
-    Serial.print(".");
-    yield();
-    nowSecs = time(nullptr);
-  }
-
-  Serial.println();
-  Serial.print("Current hour: ");
-  Serial.println(get_hour());
-}
-
-void blink()
-{
-  digitalWrite(RED, LOW);
-  delay(500);
-  digitalWrite(RED, HIGH);
-}
-
-void blink2()
-{
-  digitalWrite(RED, LOW);
-  delay(100);
-  digitalWrite(RED, HIGH);
-  delay(100);
-  digitalWrite(RED, LOW);
-  delay(100);
-  digitalWrite(RED, HIGH);
-  delay(100);
-  digitalWrite(RED, LOW);
-  delay(100);
-  digitalWrite(RED, HIGH);
 }
