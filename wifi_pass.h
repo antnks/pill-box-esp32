994c3d383f403dde628481897386d3748af85ddb
antnks <antnks@users.noreply.github.com>
2024-02-15 10:16:07 +0200

    Fixed new APIURL

--- wifi_pass.h
+++ wifi_pass.h
@@ -1,5 +1,4 @@
 int boxid = 0;
 char *WIFISSID = "mywifi";
 char *WIFIPSK = "mypass";
-char *CONFURL = "https://myserver/config";
-char *LOGURL = "https://myserver/script";
+char *APIURL = "https://myserver/script";
